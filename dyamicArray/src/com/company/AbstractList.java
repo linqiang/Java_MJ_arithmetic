73092ac8be7c52d70e18fb7a1e22e2dc3047b93c
linqiangz <zs_lynx@163.com>
2021-01-12 17:53:26 +0800

    MJ_算法

---
+++ dyamicArray/src/com/company/AbstractList.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public abstract class AbstractList<E> implements List<E>{
+    /**
+     * 元素的数量
+     */
+    protected int size;
+
+    protected void outOfBounds(int index) {
+        throw new IndexOutOfBoundsException("Index:" + index + ", Size:" + size);
+    }
+
+    protected void rangeCheck(int index) {
+        if (index < 0 || index >= size) {
+            outOfBounds(index);
+        }
+    }
+
+    protected void rangeCheckForAdd(int index) {
+        if (index < 0 || index > size) {
+            outOfBounds(index);
+        }
+    }
+    /**
+     * 是否为空
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 是否包含某个元素
+     * @param element
+     * @return
+     */
+    public boolean contains(E element) {
+        return indexOf(element) != ELEMENT_NOT_FOUND;
+    }
+
+    /**
+     * 添加元素到尾部
+     * @param element
+     */
+    public void add(E element) {
+        add(size, element);
+    }
+}
